a0b417cc4682ef95c01f539d2684b4b309c7ddec
Modchu <kawd82jstskl@yahoo.co.jp>
2013-05-02 04:55:24 +0900

    新規

---
+++ 1.4.6_7/net/minecraft/src/mod_RidingIndicatingRod.java
@@ -0,0 +1,177 @@
+package net.minecraft.src;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.util.ArrayList;
+import java.util.List;
+
+import net.minecraft.client.Minecraft;
+/*//FMLdelete
+import net.minecraft.item.*;
+*///FMLdelete
+public class mod_RidingIndicatingRod extends BaseMod {
+
+	public static int ridingIndicatingRodID = 17660;
+	public static boolean useAddChatMessage = true;
+
+	public static Item ridingIndicatingRod;
+	private static boolean DebugMessage = true;
+	private static mod_RidingIndicatingRod mod_ridingIndicatingRod;
+	private static final File cfgdir = new File(Minecraft.getMinecraftDir(), "/config/");
+	private static File whiteListfile = new File(cfgdir, ("RidingIndicatingRod_whiteList.cfg"));
+	private static File ngListfile = new File(cfgdir, ("RidingIndicatingRod_NGList.cfg"));
+	public static List<String> whiteList = new ArrayList<String>();
+	public static List<String> ngList = new ArrayList<String>();
+	private static File mainCfgfile = new File(cfgdir, ("RidingIndicatingRod.cfg"));
+
+	public static void Debug(String s)
+	{
+		if (DebugMessage)
+		{
+			System.out.println((new StringBuilder()).append("ridingIndicatingRod-").append(s).toString());
+		}
+	}
+
+	public static void mDebug(String s)
+	{
+		if (DebugMessage
+				&& !mod_ridingIndicatingRod.isRelease())
+		{
+			System.out.println((new StringBuilder()).append("ridingIndicatingRod_").append(s).toString());
+		}
+	}
+
+	public boolean isRelease() {
+		return getClass().getPackage() == null;
+	}
+
+	public String getVersion() {
+		return "1.4.6-1a";
+	}
+
+	public void load() {
+		mod_ridingIndicatingRod = this;
+		loadcfg();
+		ridingIndicatingRod = new ItemRidingIndicatingRod(
+				ridingIndicatingRodID - 256).setIconCoord(12, 6).setItemName(
+				"RidingIndicatingRod");
+		ModLoader.addName(ridingIndicatingRod, "ridingIndicatingRod");
+		ModLoader.addRecipe(new ItemStack(ridingIndicatingRod, 1),
+				new Object[] { " Y ", "YXY", " Y ", Character.valueOf('X'),
+						Item.blazeRod, Character.valueOf('Y'), Item.redstone });
+		//loadList();
+	}
+
+	public static void writerList(String[] s, File file, List<String> list) {
+		//Listファイル書き込み
+		try {
+			BufferedWriter bwriter = new BufferedWriter(new FileWriter(file));
+			for (int i = 0; i < s.length ; i++)
+			{
+				//mDebug("s[i]="+s[i]);
+				if (s[i] != null) {
+					bwriter.write(s[i]);
+					list.add(s[i]);
+					bwriter.newLine();
+				}
+			}
+			bwriter.close();
+			Debug("file new file create.");
+		} catch (Exception e) {
+			Debug("file writer fail.");
+			e.printStackTrace();
+			Debug(" ");
+		}
+	}
+/*
+	public static void loadList() {
+		// List読み込み
+		if (cfgdir.exists()) {
+			if (!whiteListfile.exists()) {
+				// whiteListファイルが無い = 新規作成
+				String s[] = {
+						"EntityCreature", "aaa", "ozelot", "wolf", "cow",
+						"chicken", "pig", "sheep", "golem", "squid",
+						"villager"
+				};
+				writerList(s, whiteListfile, whiteList);
+			} else {
+				// whiteListファイルがある
+				try {
+					BufferedReader breader = new BufferedReader(new FileReader(
+							whiteListfile));
+					String rl;
+					int i = 0;
+					while ((rl = breader.readLine()) != null) {
+						whiteList.add(rl);
+						i++;
+					}
+					breader.close();
+					mDebug("whiteList load end.");
+				} catch (Exception e) {
+					Debug("whiteList file load fail.");
+		            e.printStackTrace();
+					Debug(" ");
+				}
+			}
+			if (!ngListfile.exists()) {
+				// NGListファイルが無い = 新規作成
+				String s[] = {
+						"EntityMob", "yy"
+				};
+				writerList(s, ngListfile, ngList);
+			} else {
+				// NGListファイルがある
+				try {
+					BufferedReader breader = new BufferedReader(new FileReader(
+							ngListfile));
+					String rl;
+					int i = 0;
+					while ((rl = breader.readLine()) != null) {
+						ngList.add(rl);
+						i++;
+					}
+					breader.close();
+					mDebug("NGList load end.");
+				} catch (Exception e) {
+					Debug("NGList file load fail.");
+		            e.printStackTrace();
+					Debug(" ");
+				}
+			}
+		}
+	}
+*/
+	public static void loadcfg() {
+		// cfg読み込み
+		if (cfgdir.exists()) {
+			if (!mainCfgfile.exists()) {
+				// cfgファイルが無い = 新規作成
+				String s[] = {
+						"ridingIndicatingRodID=17660","useAddChatMessage=true"
+				};
+				RidingIndicatingRodConfig.writerConfig(mainCfgfile, s);
+			} else {
+				// cfgファイルがある
+				ridingIndicatingRodID = Integer.valueOf((RidingIndicatingRodConfig.loadConfig(mainCfgfile, "ridingIndicatingRodID", ridingIndicatingRodID)).toString());
+				useAddChatMessage = Boolean.valueOf((RidingIndicatingRodConfig.loadConfig(mainCfgfile, "useAddChatMessage", useAddChatMessage)).toString());
+				cfgMaxMinCheck();
+				String k[] = {
+						"ridingIndicatingRodID","useAddChatMessage"
+				};
+				String k1[] = {
+						""+ridingIndicatingRodID,""+useAddChatMessage
+				};
+				RidingIndicatingRodConfig.writerSupplementConfig(mainCfgfile, k, k1);
+			}
+		}
+	}
+
+	public static void cfgMaxMinCheck() {
+		if (ridingIndicatingRodID < 0) ridingIndicatingRodID = 0;
+		if (ridingIndicatingRodID > 32000) ridingIndicatingRodID = 32000;
+	}
+}
