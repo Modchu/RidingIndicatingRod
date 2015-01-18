6ffcbc21bf45095bc713a4d4800a7db12eaa5df1
modchu <kawd82jstskl@yahoo.co.jp>
2015-01-18 18:27:57 +0900

    v4

---
+++ src/main/java/modchu/ridingindicatingrod/forgeonly164/RidingIndicatingRod.java
@@ -0,0 +1,8 @@
+package modchu.ridingindicatingrod.forgeonly164;
+
+import cpw.mods.fml.common.Mod;
+
+@Mod(modid = "RidingIndicatingRod", name = "RidingIndicatingRod", version = "dummy164")
+public class RidingIndicatingRod {
+	// assets読み込み用ダミー
+}
---
+++ src/main/java/modchu/ridingindicatingrod/forgeonly180/RidingIndicatingRod.java
@@ -0,0 +1,8 @@
+package modchu.ridingindicatingrod.forgeonly180;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(modid = "RidingIndicatingRod", name = "RidingIndicatingRod", version = "dummy180")
+public class RidingIndicatingRod {
+	// assets読み込み用ダミー
+}
